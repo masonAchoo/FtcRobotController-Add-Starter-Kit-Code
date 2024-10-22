f94a1fcd1d696c80245348aaa4938071343bf665
masonAchoo <masonchu88@gmail.com>
2024-10-22 15:23:24 -0400

    none
    none

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoviceStarterBotTeleopCode.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="FTC Starter Bot Test Code", group="Test")
+
+public class NoviceStarterBotTeleopCode {
+    leftDrive =
+}
